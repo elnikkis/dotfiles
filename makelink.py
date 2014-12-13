451093c4d4ba4960b7bafe795487be42213befdc
elnikkis <noahluces@gmail.com>
2014-12-14 03:39:09 +0900

    ちゃんとホームディレクトリにリンクを張るスクリプトが完成

--- makelink.py
+++ makelink.py
@@ -8,8 +8,17 @@ dotfilesのdot.*ファイルをホームにコピーするスクリプト
 import glob
 files = glob.glob('dot.*')
 
+# このディレクトリの絶対パスを取得
+import os
+pwd = os.getcwd()
+home = os.environ['HOME']
+
+# シンボリックリンクを作成
 import subprocess
 for filename in files:
-    subprocess.call(['echo', filename])
-
+    src = os.path.join(pwd, filename)
+    dst = os.path.join(home, filename[3:])
+    op = 'ln -sin ' + src + ' ' + dst
+    print(op)
+    subprocess.call(['ln', '-sin', src, dst])
 
